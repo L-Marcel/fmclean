dceda4bf3112962e2b17e4f85fe67b09936f4a47
l-marcel <lmgh1312@gmail.com>
2023-08-12 18:24:23 -0300

    chore: changing evals

--- src/Main.lean
+++ src/Main.lean
@@ -7,7 +7,7 @@ def sampleFunction1(x) :=
 
 def result1 := sampleFunction1 4573
 
-#eval "The result of squaring the integer 4573 and adding 3 is " ++ to_string result1
+#eval "The result of sampleFunction1 4573 is " ++ to_string result1
 
 def sampleFunction2(x y) := 
   2*x*x - x + 3 + y
@@ -15,7 +15,7 @@ def sampleFunction2(x y) :=
 def result2 := 
   sampleFunction2 (7 + 4) 8
 
-#eval "The result of applying the 2nd sample function to (7 + 4) is " ++ to_string result2
+#eval "The result of applying the 2nd sample function to (7 + 4) 8 is " ++ to_string result2
 
 def sampleFunction3(x) :=
   if x > 100 then
